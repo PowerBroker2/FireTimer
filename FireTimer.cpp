64aa5564eae55752fe6e3a8b7ce960ce79d3e1fa
PB2 <gitstuff2@gmail.com>
2020-01-25 16:01:22 -0500

    Add files via upload

---
+++ FireTimer.cpp
@@ -0,0 +1,144 @@
+#include "FireTimer.h"
+
+
+
+
+/*
+ void FireTimer::begin(ulong timeout, bool micros)
+
+ Description:
+ ------------
+  * Constructor for the FireTimer Class
+
+ Inputs:
+ -------
+  * uint8_t timeout- Timeout in ms (or us if micros flag is set)
+  * bool micros - Set timeout units to us
+
+ Return:
+ -------
+  * void
+*/
+void FireTimer::begin(ulong timeout, bool micros)
+{
+	us = micros;
+	update(timeout);
+}
+
+
+
+
+/*
+ bool FireTimer::update(ulong timeout)
+
+ Description:
+ ------------
+  * Update timer's timeout and reinitialize the timer
+
+ Inputs:
+ -------
+  * ulong timeout - Number of ms/us to fire after
+
+ Return:
+ -------
+  * void
+*/
+void FireTimer::update(ulong timeout)
+{
+	period = timeout;
+	start();
+}
+
+
+
+
+/*
+ bool FireTimer::start()
+
+ Description:
+ ------------
+  * Initializes/reinitializes timer
+
+ Inputs:
+ -------
+  * void
+
+ Return:
+ -------
+  * void
+*/
+void FireTimer::start()
+{
+	if (us)
+		timeBench = micros();
+	else
+		timeBench = millis();
+}
+
+
+
+
+/*
+ bool FireTimer::reset()
+
+ Description:
+ ------------
+  * Resets timer
+
+ Inputs:
+ -------
+  * void
+
+ Return:
+ -------
+  * void
+*/
+void FireTimer::reset()
+{
+	timeBench += period;
+}
+
+
+
+
+/*
+ bool FireTimer::fire()
+
+ Description:
+ ------------
+  * Determine if enough time has elapsed - handles
+  millis() and micros() rollover
+
+ Inputs:
+ -------
+  * void
+
+ Return:
+ -------
+  * bool - Whether or not enough time has elapsed
+*/
+bool FireTimer::fire()
+{
+	ulong currentTime;
+
+	// Update current time in user specified units
+	if (us)
+		currentTime = micros();
+	else
+		currentTime = millis();
+
+	// Handle overflow
+	if (currentTime < timeBench)
+		timeDiff = (UL_MAX - timeBench) + currentTime;
+	else
+		timeDiff = currentTime - timeBench;
+
+	// Determine if enough time has passed
+	if (timeDiff >= period)
+	{
+		reset();
+		return true;
+	}
+
+	return false;
+}
---
+++ FireTimer.h
@@ -0,0 +1,34 @@
+#pragma once
+#include "Arduino.h"
+
+
+
+
+typedef unsigned long ulong;
+
+const ulong UL_MAX = 4294967295;
+
+
+
+
+class FireTimer
+{
+public: // <<---------------------------------------//public
+	bool us = false;
+	ulong timeDiff = 0;
+
+
+
+
+	void begin(ulong timeout, bool micros=false);
+	void update(ulong timeout);
+	void reset();
+	bool fire();
+
+
+
+
+private: // <<---------------------------------------//private
+	ulong timeBench = 0;
+	ulong period    = 0;
+};
