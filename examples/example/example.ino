3df0871735ec3eb89f4a6687f39b90a67cdc1633
PB2 <gitstuff2@gmail.com>
2020-01-25 16:01:56 -0500

    Create example.ino

---
+++ examples/example/example.ino
@@ -0,0 +1,21 @@
+#include "FireTimer.h"
+
+FireTimer msTimer;
+FireTimer usTimer;
+
+void setup()
+{
+  Serial.begin(115200);
+
+  msTimer.begin(1000);
+  usTimer.begin(1000000, true);
+}
+
+void loop()
+{
+  if(msTimer.fire())
+    Serial.println("ms");
+
+  if(usTimer.fire())
+    Serial.println("us");
+}
